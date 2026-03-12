bfca6d8ac23a1a0c7e26a8ab25d2abb58befd918
marki <mbertoglio@alumno.huergo.edu.ar>
2026-03-12 14:16:28 -0300

    Update ej14.4.py

--- RepasoPhyton/ej14.4.py
+++ RepasoPhyton/ej14.4.py
@@ -1,5 +1,17 @@
 lista = list(range(1,10))
 
-for numero in lista:
-    if numero == 1:
-        
+for i in lista:
+    if i == 1:
+        print(f"{i}ero")
+    elif i == 2:
+        print(f"{i}do")
+    elif i == 3:
+        print(f"{i}ero")
+    elif i >= 4 and i <= 6:
+        print(f"{i}to")
+    elif i == 7:
+        print(f"{i}mo")
+    elif i == 8:
+        print(f"{i}vo")
+    else:
+        print(f"{i}no")
