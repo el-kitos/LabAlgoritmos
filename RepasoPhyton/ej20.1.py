6a1085d1ee7accf917448f42964f0d3843dc8180
marki <mbertoglio@alumno.huergo.edu.ar>
2026-03-18 08:41:34 -0300

    summary

---
+++ RepasoPhyton/ej20.1.py
@@ -0,0 +1,9 @@
+pedidos_sandwiches = ["jamon", "aaaa", "salame"]
+sandwiches_terminados = []
+
+for sandwich in pedidos_sandwiches:
+    print(f"Tu sandiwch de {sandwich}")
+    sandwiches_terminados.append(sandwich)
+    
+
+print(f"Estos son los sandwiches terminados: {sandwiches_terminados}")
