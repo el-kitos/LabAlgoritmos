3c782c1d1eaa449d885fa06b13f08e9a1a993f48
marki <mbertoglio@alumno.huergo.edu.ar>
2026-03-19 15:48:48 -0300

    Update ej27.3.py

---
+++ ej27.3.py
@@ -0,0 +1,14 @@
+class Usuario:
+    def __init__(self, nombre, apellido, email, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.edad = edad
+
+    def describir_usuario(self):
+        print(f"El usario se llama {self.nombre} {self.apellido}, su email es {self.email} y tiene {self.edad} años ")
+    
+    def saludar(self):
+        print(f"hola {self.nombre}")
+    
+    juanjo = Usuario("Juanjo", "Alfonso","juanjoalfonso@gmail", "8")
