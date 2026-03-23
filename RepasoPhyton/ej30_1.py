144ec6896a79b43d34d7220758a4239f23ee879e
markitos <mbertoglio@alumno.huergo.edu.ar>
2026-03-23 10:07:57 -0300

    summary

---
+++ RepasoPhyton/ej30_1.py
@@ -0,0 +1,5 @@
+from ej28_1 import Restaurante
+
+Marcos = Restaurante("Hola", "Los mariscos")
+
+Marcos.describir_restaurante()
---
+++ RepasoPhyton/ej30_2.py
@@ -0,0 +1,5 @@
+from ej29_3 import Usuario, Privilegios, Administrador
+
+admin = Administrador("Juanjo", "Alfonso", "juanjoalfonso@gmail", 8)
+
+admin.privilegios.mostrar_privilegios()
